5ee04a3a8ac161590d13f98e73c11e1d1b768d99
Chartok <m.bhimjee@outlook.com>
2023-09-03 15:19:25 -0500

    added style to contact page; done?

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,34 +1,74 @@
 // components
-import BlkHole from '../../components/BlkHole'
+import Avatar2 from '../../components/Avatar2';
+import ParticlesContainer from '../../components/ParticlesContainer';
 
 // icons
-import { BsArrowRight } from 'react-icons/bs'
+import { BsArrowRight } from 'react-icons/bs';
 
 // framer motion
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
 // variants
-import { fadeIn } from '../../variants'
+import { fadeIn } from '../../variants';
 
 const Contact = () => {
   return (
     <div className='h-full bg-primary/30'>
       <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
-      {/* text & form */}
-      <div className='flex flex-col w-full max-w-[700px]'>
-        {/* text */}
-        <motion.h2 variants={fadeIn('up', 0.2)} initial='hidden' animate='show' exit='hidden' className='h2 text-center mb-12'>
-          Connect <span className='text-accent'> with me.</span>
-        </motion.h2>
-        {/* form */}
-        <motion.form variants={fadeIn('up', 0.4)} initial='hidden' animate='show' exit='hidden' className='flex-1 flex flex-col gap-6 w-full mx-auto'>
-          {/* input group */}
-          <div className='flex gap-x-6 w-full'>
-            <p className='border-solid p-4 rounded border-2 border-accent '>Drop me a line <span className='text-accent'>@</span> m.bhimjee@outlook.com</p>
+        {/* text */ }
+        <div className='flex flex-col w-full max-w-[700px] justify-center'>
+          <motion.h2
+            variants={ fadeIn( 'up', 0.2 ) }
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h2 mb-12'>
+            Connect <span className='text-accent'> with me.</span>
+          </motion.h2>
+
+        <motion.div
+          variants={ fadeIn( 'left', 0.4 ) }
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='w-full max-w-[350px] border-solid p-8 rounded border-2 border-accent text-center'>
+          <p>Drop a
+            <span className='text-accent'> line.</span>
+          </p>
+          <motion.hr
+            variants={ fadeIn('delay', 1)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+          />
+          <motion.h2
+            variants={ fadeIn( 'delay', 2.5 ) }
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+          >
+            <span className='text-accent'>m.bhimjee</span>@outlook.com</motion.h2>
+        </motion.div>
+
           </div>
-        </motion.form>
-      </div>
-    
+        {/* image */ }
+        <div className='w-[1280px] h-full absolute bottom-0 right-0'>
+          {/* bg-img */ }
+          <div
+            className='bg-none xl:bg-rorschach -rotate-45 xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-darken z-0 translate-x-[250px]'></div>
+          {/* particles */ }
+          <ParticlesContainer />
+          {/* avatar */ }
+          <motion.div
+            variants={ fadeIn( 'up', 0.5 ) }
+            initial='hidden'
+            animate='show'
+            exit='exit'
+            transition={ { duration: 1, ease: 'easeInOut' } }
+            className='w-[40%] max-w-[462px] max-h-[540px] absolute -bottom-32 lg:bottom-0 lg:right-[7%]'>
+            <Avatar2 />
+          </motion.div>
+        </div>
       </div>
     </div>
   );
