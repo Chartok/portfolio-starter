2db392725408dddb2505e404416c61c9d6edeeb3
Chartok <m.bhimjee@outlook.com>
2023-09-06 18:25:29 -0500

    mvp

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -44,7 +44,7 @@ export const workSlider = {
         },
         {
           title: 'Survey',
-          path: '/city_skyline.jpg',
+          path: '/city_skyilne.jpg',
           URL: 'https://chartok.github.io/code-survey/'
         },
       ],
@@ -78,7 +78,7 @@ const WorkSlider = () => {
         clickable: true,
       }}
       modules={[Pagination]}
-      className='h-[280px] sm:h-[480px]'
+      className='h-[280px] sm:h-[463px]'
     >
       {workSlider.slides.map((slide, index) => {
         return (
@@ -86,23 +86,23 @@ const WorkSlider = () => {
             <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
               {slide.images.map((image, index) => {
                 return (
-                  <div 
-                    className='relative rounded-lg overflow-hidden mb-10 flex items-center justify-center group' 
+                  <div
+                    className='relative rounded-lg overflow-hidden flex items-center justify-center group'
                     key={index}
                   >
-                    <div className='flex items-center justify-center relative overflow-hidden'>
+                    <div className='flex items-center justify-center relative overflow-hidden group w-[500px] h-[500px]'>
                       {/* image */}
-                      <Image src={image.path} width={500} height={300} alt={image.title} />
+                      <Image src={image.path} alt={image.title} sizes='500px' style={{ objectFit: 'contain' }} fill />
                       {/* overlay gradient */}
                       <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#0ef] to-[#07f] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
                       {/* title */}
-                      <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+                      <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-16 group-hover:xl:-translate-y-20 transition-all duration-300'>
                         <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
                           <div>
                             <a href={image.URL} target='_blank' rel='noopener noreferrer'>
-                            {/* title */}
-                            <div className='delay-100'>PROJECT: </div>
-                            <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>{image.title}</div>
+                              {/* title */}
+                              <div className='delay-100'>PROJECT: </div>
+                              <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>{image.title}</div>
                             </a>
                           </div>
                           {/* icon */}
