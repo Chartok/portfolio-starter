5c78abbce84d00792c32b0fdf5688a5bcd0feaa8
Chartok <m.bhimjee@outlook.com>
2023-09-11 14:31:35 -0500

    worked through responsiveness in work component

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -94,10 +94,10 @@ const WorkSlider = () => {
                       {/* image */}
                       <Image src={image.path} alt={image.title} width={500} height={500} style={{ width: 'auto', height: 'auto', objectFit: 'contain' }} />
                       {/* overlay gradient */}
-                      <div className='absolute contain inset-0 bg-gradient-to-b from-transparent via-[#012346] to-[#939393] opacity-0 group-hover:opacity-40 transition-all duration-700'></div>
+                      <div className='absolute contain inset-0 bg-gradient-to-b from-transparent via-[#012346] to-[#939393] opacity-0 group-hover:opacity-60 transition-all duration-700'></div>
                       <div className='absolute bottom-0 translate-y-full  group-hover:-translate-y-40 group-hover:xl:-translate-y-40 transition-all duration-300'>
                       {/* text in title container*/}
-                        <div className='flex gap-x-2 text-[16px] tracking-[0.4rem]'>
+                        <div className='flex gap-x-2 text-[14px] tracking-[0.2rem]'>
                           <div>
                             <a href={image.URL} target='_blank' rel='noopener noreferrer'>
                               {/* title event*/}
